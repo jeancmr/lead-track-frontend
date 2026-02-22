2c1f122bf3a1ff82265bc003664ac0490206431a
jeancmr <jean.madiedo08@gmail.com>
2026-02-21 21:13:40 -0500

    build: add tailwind and shadcn

--- src/LeadTrackApp.tsx
+++ src/LeadTrackApp.tsx
@@ -1,3 +1,10 @@
+import { Button } from './components/ui/button';
+
 export const LeadTrackApp = () => {
-  return <div>LeadTrackApp</div>;
+  return (
+    <div>
+      <h1>LeadTrackApp</h1>
+      <Button variant={'destructive'}>Click here</Button>
+    </div>
+  );
 };
